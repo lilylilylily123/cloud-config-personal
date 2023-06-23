341e90b097813a2ea10733e3c64ba51290e55ff0
Lily G <aksg656@icloud.com>
2023-06-23 15:11:16 +0200

    client?

---
+++ client/app.ts
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+const port = 5173;
+
+const jsonParser = bodyParser.json();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.post('/config', jsonParser, (req, res) => {
+    const config = req.body;
+    console.log(req.body)
+    res.send(config);
+})
+
+app.listen(port, () => {
+    console.log("server running on port " + port);
+})
